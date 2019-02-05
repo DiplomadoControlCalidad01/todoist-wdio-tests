b36b1c536253adcc043a14e62b436077dead3ab7
ICSC <ICSC@fundacion-jala.local>
2019-02-04 20:57:53 -0400

    adding new pages

---
+++ e2e/pages/topBar.po.js
@@ -0,0 +1,20 @@
+const CommonActions = require('../core/CommonActions');
+const {explicit} = require ('../../environment').timeout;
+
+class TopBar{
+
+    constructor(){
+        this.SearchField = '.quick_find';
+        this.AddButton   = 'quick_add_task_holder';
+
+    }
+
+    clickAddTaskButton(){
+        CommonActions.click(this.AddButton);
+    }
+
+    setSearchField(item){
+        CommonActions.setValue(this.SearchField, item);
+        CommonActions.enter(this.SearchField);
+    }
+}
