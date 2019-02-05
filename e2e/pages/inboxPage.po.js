f93c9e0688a86608435906834e2aa40c0aafd4b8
eduardoo16 <eduardoo.roc.tor@gmail.com>
2019-02-05 18:34:46 -0400

    Adding inbocPage.po

--- e2e/pages/inboxPage.po.js
+++ e2e/pages/inboxPage.po.js
@@ -8,6 +8,11 @@ class InboxPage{
     constructor(){
         this.addTaskInbox  = '.action_add_item';
         this.completedtasks= '.history_icon';
+        this.setPriority   = '.icon_priority';
+        this.setProject    = '.form_action_icon[data-track="task_form|project"]';
+    }
+
+    addNewTaskInbox(){
 
 
     }
